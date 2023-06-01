583dc7705e21addf3f43b85c3d155809cb37f88d
Armany <58051843+armanyfelix@users.noreply.github.com>
2023-06-01 03:11:43 -0700

    proyects slides ready

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Btn from '../styles/customBtn.module.css';
 // import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
-import { useState, FC, Fragment } from 'react';
+import { FC } from 'react';
 // import marquee from '../styles/marquee.module.css'
 // import github from '../public/icons/github.svg'
 // import ProyectsData from './proyectsData.module.js'
@@ -17,25 +17,19 @@ const data = [
     title: 'Civity CMS',
     description:
       'Civity is a property management software that offers you management tools that facilitate a clear and organized administration. From billing automation, common area reservations, voting, guest control and mouch more. Your administration will be more efficient, streamline access to your community information, you will save time and strengthen the management of your Condominiums and Buildings.',
+    url: 'https://admin.civity.mx/',
     stack: [],
-    images: [
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-    ],
+    type: 'web',
+    images: ['civity_cms_1.png', 'civity_cms_2.png', 'civity_cms_3.png', 'civity_cms_4.png'],
   },
   {
     title: 'Civity App',
     description:
       "The Civity app is designed for residents of condominiums managed by Civity CMS. With rhis app, you can pay your bills, reserve common areas, vote on condo decisions, report incidents, and stay on top of what's happening in your community. It is the easiest way to manage your property and access all the services of your condominium.",
+    url: 'https://app.civity.mx/',
     stack: [],
-    images: [
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-      '/images/proyects/admin.civity.home.jpeg',
-    ],
+    type: 'mobile',
+    images: ['civity_app_1.png', 'civity_app_2.png', 'civity_app_3.png', 'civity_app_4.png'],
   },
   // {
   //   name: 'Website and Store',
@@ -46,15 +40,15 @@ const data = [
   //   images: [],
   //   tecs: ['React', 'Redux', 'MongoDB', 'Express', 'Stripe', 'Taiwindcss'],
   // },
-  {
-    name: "Car's bodyshop",
-    url: 'https://felixgarages.netlify.app/',
-    source: 'https://github.com/armanyfelix/felixgarage',
-    description:
-      "This is a landing page for a car's bodyshop that has a gallery with they work and different contact methods.",
-    images: [],
-    stack: ['React', 'Tailwindcss', 'Express', 'Messenger-chat', 'Google-Maps-api'],
-  },
+  // {
+  //   name: "Car's bodyshop",
+  //   url: 'https://felixgarages.netlify.app/',
+  //   source: 'https://github.com/armanyfelix/felixgarage',
+  //   description:
+  //     "This is a landing page for a car's bodyshop that has a gallery with they work and different contact methods.",
+  //   images: [],
+  //   stack: ['React', 'Tailwindcss', 'Express', 'Messenger-chat', 'Google-Maps-api'],
+  // },
 ];
 
 // const handleProyectScroll = (e: any) => {
@@ -62,10 +56,8 @@ const data = [
 // }
 
 const Proyects: FC<ProyectsProps> = () => {
-  const [current] = useState(0);
-
   return (
-    <section id="proyects" className=" w-full min-h-screen p-8 md:pt-24 mt-40">
+    <section id="proyects" className="min-h-screen p-8 md:pt-24 mt-40">
       <div className="flex justify-between items-center lg:px-52 sm:m-2 md:m-3 lg:m-4 xl:m-5">
         <h1 className="lg:w-3/5 text-white lg:text-8xl md:text-5xl sm:text-3xl text-3xl items-center">
           Proyects
@@ -74,66 +66,48 @@ const Proyects: FC<ProyectsProps> = () => {
           <span className="font-mono px-6  text-2xl md:text-4xl">See all</span>
         </Link>
       </div>
-      <div className=" ">
-        {/* <button
-          type="button"
-          onClick={() => setCurrent(current === 0 ? data.length - 1 : current - 1)}
-          className="hover:bg-opacity-10 hover:bg-white mt-16 pl-0 md:mr-10 rounded-2xl scale-50 lg:scale-100"
-        >
-          <ChevronLeftIcon className="w-28 h-28 text-white mr-2" />
-        </button> */}
+      <div className="carousel w-full p-20">
         {data.map((p, i) => (
-          <Fragment key={i}>
-            <div
-              key={i}
-              // onScroll={(e) => handleProyectScroll(e)}
-              className={`${
-                current === i ? 'lg:flex' : 'hidden'
-              }   mt-16 p-6 md:p-10 md:min-h-[80vh] md:max-h-[80vh] justify-between rounded-2xl ring-2 ring-slate-100 bg-black shadow-xl backdrop-filter backdrop-blur backdrop-brightness-90 bg-opacity-20`}
-            >
+          <div
+            key={`proyect${i}`}
+            id={`proyect${i}`}
+            className="carousel-item relative w-full md:min-h-[80vh] md:max-h-[80vh] pt-20 px-20"
+          >
+            <div className="lg:flex justify-between p-6 md:p-10 rounded-2xl ring-2 ring-slate-100 bg-black shadow-xl backdrop-filter backdrop-blur backdrop-brightness-90 bg-opacity-20">
               <div className="lg:w-2/5 md:p-5">
                 <Link href="https://www.civity.mx" target="_blank" className="text-6xl">
                   {p.title}
                 </Link>
                 <p className="text-justify mt-6">{p.description}</p>
               </div>
-              <div className="md:w-3/5 md:grid md:grid-cols-2 mx-auto gap-5 mt-8 flex overflow-x-auto 5">
+              <div
+                className={`md:w-3/5 md:grid ${
+                  p.type === 'web' ? 'md:grid-cols-2' : 'grid-flow-col'
+                } mx-auto gap-5 my-auto flex overflow-auto`}
+              >
                 {p.images?.map((url, i) => (
-                  <Image src={url} key={i} alt="" width={500} height={500} className="rounded-lg" />
+                  <Image
+                    src={`/images/proyects/${url}`}
+                    key={i}
+                    alt=""
+                    width={900}
+                    height={700}
+                    className="rounded-xl max-h-[70vh] w-auto mx-auto"
+                  />
                 ))}
               </div>
             </div>
-            <div>
-              <div
-                className={`${
-                  current === i ? 'bg-red-500' : 'bg-purple-500'
-                } rounded-full flex items-center justify-center`}
-              />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 pt-20">
+              <a href={`#proyect${i === 0 ? data.length - 1 : i - 1}`} className="btn btn-circle">
+                ❮
+              </a>
+              <a href={`#proyect${i === data.length - 1 ? 0 : i + 1}`} className="btn btn-circle">
+                ❯
+              </a>
             </div>
-          </Fragment>
+          </div>
         ))}
-        {/* <button
-          type="button"
-          onClick={() => setCurrent(current === data.length - 1 ? 0 : current + 1)}
-          className="hover:bg-opacity-10 hover:bg-white mt-16 md:ml-10 rounded-2xl scale-50 lg:scale-100"
-        >
-          <ChevronRightIcon className="w-28 h-28 text-white" />
-        </button> */}
       </div>
-
-      {/* <div className="mt-12 grid xl:grid-cols-3 xl:mx-20 sm:grid-cols-2 grid-cols-1 items-center justify-center">
-        {ProyectsData.map((data) => (
-          <ProyectCard
-            key={data.name}
-            name={data.name}
-            url={data.url}
-            source={data.source}
-            description={data.description}
-            image={data.image}
-            tecs={data.tecs}
-          />
-        ))}
-      </div> */}
     </section>
   );
 };
